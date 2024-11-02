e919de77ae67cc204090959937d4f8dbf0a8d62c
alex <alex@slash-root.com>
2024-11-02 16:23:45 +0000

    fixed errors

--- docker/certbot/certify-init.sh
+++ docker/certbot/certify-init.sh
@@ -1,38 +1,19 @@
-#!/bin/bash
-
-'''
-checks if the DOMAIN and EMAIL environment variables are set, waits for the proxy to be available, and then uses certbot to obtain a certificate for the specified domain.
-'''
+#!/bin/sh
 
 set -e
 
-# Check if DOMAIN and EMAIL are set
-if [ -z "$DOMAIN" ]; then
-    echo "Error: DOMAIN environment variable is not set."
-    exit 1
-fi
-
-if [ -z "$EMAIL" ]; then
-    echo "Error: EMAIL environment variable is not set."
-    exit 1
-fi
-
 until nc -z proxy 80; do
-    echo "Waiting for proxy to be available"
+    echo "Waiting for proxy..."
     sleep 5s & wait ${!}
 done
 
 echo "Getting certificate..."
-if ! certbot certonly \
+
+certbot certonly \
     --webroot \
     --webroot-path "/vol/www/" \
     -d "$DOMAIN" \
     --email "$EMAIL" \
     --rsa-key-size 4096 \
     --agree-tos \
-    --noninteractive; then
-    echo "Error: certbot failed to obtain the certificate."
-    exit 1
-fi
-
-echo "Certificate obtained successfully."
+    --noninteractive
