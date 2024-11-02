c9f1ed35d9ecbdfaff6d7e6d092df272d5322f62
alex <alex@slash-root.com>
2024-11-02 16:42:17 +0000

    fixed errors

--- docker/proxy/run.sh
+++ docker/proxy/run.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-'''
-This script is used to generate the dhparams.pem file and check for the fullchain.pem file.
-'''
-
 set -e
 
 echo "Checking for dhparams.pem"
-if [ ! -f "/vol/nginx/ssl-dhparams.pem" ]; then
+if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
   echo "Generating dhparams.pem"
   openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
 fi
-
-# avoid replacing these with envsubst
-export host=\$host
-export request_uri=\$request_uri
 
 echo "Check for fullchain.pem"
 if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
